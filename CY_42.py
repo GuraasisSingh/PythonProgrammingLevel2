d339997bd56aef162ac8609d475c46e50978e132
Guraasis Singh <guraasiss@gmail.com>
2022-12-13 23:12:50 +0530

    bar plots... with pics

--- CY_42.py
+++ CY_42.py
@@ -91,6 +91,8 @@ plt.show()
 
 
 #  Categorial Plots: 
+
+
 # Barplot
 # A barplot is basically used to aggregate the categorical data according to some methods and by default its the mean. It can also be understood a
 # s a visualization of the group by action. To use this plot we choose a categorical column for the x axis and a numerical column for the y axis and 
@@ -100,7 +102,14 @@ plt.show()
 
 # barplot([x, y, hue, data, order, hue_order, …])
 
+# seaborn.barplot() method
+# A barplot is basically used to aggregate the categorical data according to some methods and by default it’s the mean. 
+# It can also be understood as a visualization of the group by action. To use this plot we choose a categorical column for the x-axis and 
+# a numerical column for the y-axis,
+# and we see that it creates a plot taking a mean per categorical column.
 
+
+'''
 import matplotlib.pyplot as plt
 import seaborn as sns
 #import done to avoid warnings 
@@ -119,3 +128,81 @@ import numpy as np
 sns.barplot(x ='sex', y ='total_bill', data = df, 
             palette ='plasma', estimator = np.std)
 plt.show()
+'''
+# import seaborn as sns
+# import matplotlib.pyplot as plt
+ 
+# # read a titanic.csv file
+# # from seaborn library
+
+# df = sns.load_dataset('titanic')
+ 
+# # who v/s fare barplot 
+# sns.barplot(x = 'who',
+#             y = 'fare',
+#             data = df) 
+# plt.show()
+
+# import seaborn as sns
+# import matplotlib.pyplot as plt
+# import numpy as np
+# # read a titanic.csv file
+# # from seaborn library
+# df = sns.load_dataset('titanic')
+# sns.barplot(x = 'who',
+#             y = 'fare',
+#             palette='Blues',
+#             estimator=np.median,
+#             hue = 'class',
+#             data = df)
+# plt.show()
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+df = sns.load_dataset('titanic')
+sns.barplot(x='who',
+            y = 'fare',
+            data=df)
+plt.show()
+
+
+'''
+ Pairplot
+
+# The Seaborn Pairplot function allows the users to create an axis grid via which each numerical variable stored 
+# in data is shared across the X- and Y-axis in the structure of columns and rows.
+# We can create the Scatter plots in order to display the pairwise relationships in addition to the distribution plot displaying the data distribution in the column diagonally.
+
+# The pairplot() function can also be used to showcase the subset of variables, or we can plot different types of variables on rows and columns.
+
+import seaborn as sbn  
+import matplotlib.pyplot as plt  
+# loading the dataset using the seaborn library  
+mydata = sbn.load_dataset('penguins')  
+# pairplot with the hue = gender parameter  
+sbn.pairplot(mydata, hue = 'gender')  
+# displaying the plot  
+plt.show()
+
+Parameters of Pairplot function:
+
+# data: The data parameter accepts the data depending on the visualization to be plotted. The values can be in terms of DataFrame, Array, or List of Arrays.
+
+# hue_order, order: The hue_order or simply order parameter is the order for categorical variables utilized in the plot. The values for this parameter can be the lists of strings.
+
+# scale: The scale parameter is used for scaling the plot. This parameter takes more than values for usage, such as area, count, or width.
+
+# scale_hue: The scale_hue parameter takes the Boolean value in order to determine whether the scale is estimated within each level of the major grouping variable for TRUE or across all the violins on the plot for FALSE.
+
+# gridsize: The gridsize parameter takes the integer value to calculate the kernel density for the plot.
+# inner: The inner parameter allows the users to define the inner points of a violin plot. This parameter takes the values such as box, point, quartile, stick, or None.
+# orient: The orient parameter allows the user to determine the plot's orientation. The orientation can be either vertical, denoted by 'v' or horizontal, denoted by 'h', respectively.
+
+# linewidth: The linewidth parameter takes the float integer as its value to determine the width of the grey lines utilized within the plot.
+
+# color: The color parameter allows the user to specify the range of color for all the data elements of the plot. The value for this parameter can be matplotlib color.
+
+# palette: The palette parameter is used to define the colors utilized for each level of the plot with a variety of hues.
+
+# ax: The ax parameter is used to define the axes on which the plot will be constructed. The value for this parameter can be matplotlib Axes.
+'''
